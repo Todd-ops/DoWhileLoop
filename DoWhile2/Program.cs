68f13f120b4281d654e35944a6e4dbc1833059fa
Todd Patton <64681278+Todd-ops@users.noreply.github.com>
2020-06-23 19:18:14 -0500

    Add project files.

---
+++ DoWhile2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Transactions;
+
+namespace DoWhile2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool retry = true;
+            Random random = new Random();
+            do
+            {
+                int randomNumber = random.Next(1, 50);
+                while (true)
+                {
+                    Console.WriteLine("Guess a number from 1 - 50");
+                    var myNum = int.Parse(Console.ReadLine());
+                    if (myNum > randomNumber)
+                    {
+                        Console.WriteLine("Too High!");
+                    }
+                    else if (myNum < randomNumber)
+                    {
+                        Console.WriteLine("Too Low!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lucky Guess!");
+                    }
+
+                }
+            } while (retry);
+        }
+    }
+}
